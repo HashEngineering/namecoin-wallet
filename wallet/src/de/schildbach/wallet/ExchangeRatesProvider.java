34090a53cac7c6ae17f0a933a58b411462125476
HashEngineering <hashengineeringsolutions@gmail.com>
2016-06-11 23:24:24 -0700

    Updates for new exchange for rates.  added native for scrypt.  updated checkpoints.

--- wallet/src/de/schildbach/wallet/ExchangeRatesProvider.java
+++ wallet/src/de/schildbach/wallet/ExchangeRatesProvider.java
@@ -273,84 +273,86 @@ public class ExchangeRatesProvider extends ContentProvider
 		throw new UnsupportedOperationException();
 	}
 
-    private static Object getCoinValueBTC_poloniex()
-    {
+
+
+	private static Object getCoinValueBTC_poloniex()
+	{
 
 
 
 
-        //final Map<String, ExchangeRate> rates = new TreeMap<String, ExchangeRate>();
-        // Keep the LTC rate around for a bit
-        Double btcRate = 0.0;
-        String currencyCryptsy = CoinDefinition.cryptsyMarketCurrency;
-        String urlCryptsy =  "https://poloniex.com/public?command=returnTradeHistory&currencyPair="+CoinDefinition.cryptsyMarketCurrency +"_" + CoinDefinition.coinTicker;
+		//final Map<String, ExchangeRate> rates = new TreeMap<String, ExchangeRate>();
+		// Keep the LTC rate around for a bit
+		Double btcRate = 0.0;
+		String currencyCryptsy = CoinDefinition.cryptsyMarketCurrency;
+		String urlCryptsy =  "https://poloniex.com/public?command=returnTradeHistory&currencyPair="+CoinDefinition.cryptsyMarketCurrency +"_" + CoinDefinition.coinTicker;
 
 
 
 
-        try {
-            // final String currencyCode = currencies[i];
-            final URL URLCryptsy = new URL(urlCryptsy);
-            final HttpURLConnection connectionCryptsy = (HttpURLConnection)URLCryptsy.openConnection();
-            connectionCryptsy.setConnectTimeout(Constants.HTTP_TIMEOUT_MS * 2);
-            connectionCryptsy.setReadTimeout(Constants.HTTP_TIMEOUT_MS * 2);
-            connectionCryptsy.connect();
+		try {
+			// final String currencyCode = currencies[i];
+			final URL URLCryptsy = new URL(urlCryptsy);
+			final HttpURLConnection connectionCryptsy = (HttpURLConnection)URLCryptsy.openConnection();
+			connectionCryptsy.setConnectTimeout(Constants.HTTP_TIMEOUT_MS * 2);
+			connectionCryptsy.setReadTimeout(Constants.HTTP_TIMEOUT_MS * 2);
+			connectionCryptsy.connect();
 
-            final StringBuilder contentCryptsy = new StringBuilder();
+			final StringBuilder contentCryptsy = new StringBuilder();
 
-            Reader reader = null;
-            try
-            {
-                reader = new InputStreamReader(new BufferedInputStream(connectionCryptsy.getInputStream(), 1024));
-                Io.copy(reader, contentCryptsy);
-                //final JSONObject head = new JSONObject(contentCryptsy.toString());
-                //JSONObject returnObject = head.getJSONObject("return");
-                //JSONObject markets = returnObject.getJSONObject("markets");
-                //JSONObject coinInfo = head.getJSONObject(CoinDefinition.cryptsyMarketCurrency +"_" + CoinDefinition.coinTicker);
-
-
+			Reader reader = null;
+			try
+			{
+				reader = new InputStreamReader(new BufferedInputStream(connectionCryptsy.getInputStream(), 1024));
+				Io.copy(reader, contentCryptsy);
+				//final JSONObject head = new JSONObject(contentCryptsy.toString());
+				//JSONObject returnObject = head.getJSONObject("return");
+				//JSONObject markets = returnObject.getJSONObject("markets");
+				//JSONObject coinInfo = head.getJSONObject(CoinDefinition.cryptsyMarketCurrency +"_" + CoinDefinition.coinTicker);
 
 
 
-                JSONArray recenttrades = new JSONArray(contentCryptsy.toString());//coinInfo.getJSONArray("recenttrades");
-
-                double btcTraded = 0.0;
-                double coinTraded = 0.0;
-
-                for(int i = 0; i < recenttrades.length(); ++i)
-                {
-                    JSONObject trade = (JSONObject)recenttrades.get(i);
-
-                    btcTraded += trade.getDouble("total");
-                    coinTraded += trade.getDouble("amount");
-
-                }
-
-                Double averageTrade = btcTraded / coinTraded;
 
 
+				JSONArray recenttrades = new JSONArray(contentCryptsy.toString());//coinInfo.getJSONArray("recenttrades");
 
-                if(currencyCryptsy.equalsIgnoreCase("BTC")) btcRate = averageTrade;
+				double btcTraded = 0.0;
+				double coinTraded = 0.0;
 
-            }
-            finally
-            {
-                if (reader != null)
-                    reader.close();
-            }
-            return btcRate;
-        }
-        catch (final IOException x)
-        {
-            x.printStackTrace();
-        }
-        catch (final JSONException x)
-        {
-            x.printStackTrace();
-        }
+				for(int i = 0; i < recenttrades.length(); ++i)
+				{
+					JSONObject trade = (JSONObject)recenttrades.get(i);
 
-        return null;
-    }
+					btcTraded += trade.getDouble("total");
+					coinTraded += trade.getDouble("amount");
+
+				}
+
+				Double averageTrade = btcTraded / coinTraded;
+
+
+
+				if(currencyCryptsy.equalsIgnoreCase("BTC")) btcRate = averageTrade;
+
+			}
+			finally
+			{
+				if (reader != null)
+					reader.close();
+			}
+			return btcRate;
+		}
+		catch (final IOException x)
+		{
+			x.printStackTrace();
+		}
+		catch (final JSONException x)
+		{
+			x.printStackTrace();
+		}
+
+		return null;
+	}
     private static Object getCoinValueBTC()
     {
 
@@ -508,7 +510,7 @@ public class ExchangeRatesProvider extends ContentProvider
 
             Double btcRate = 0.0;
             boolean cryptsyValue = true;
-            Object result = getCoinValueBTC();
+            Object result = getCoinValueBTC_poloniex();
 
             if(result == null)
             {
